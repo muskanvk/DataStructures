65eb13587b35b4561bfd0f03f504fa2a3f5c7ba8
Muskan Kumar <muskanvk97@gmail.com>
2020-09-11 22:43:17 +0530

    Add solution'

---
+++ Missing_Number.cpp
@@ -0,0 +1,44 @@
+//Question 5 : Design the logic to find the missing number in a sorted array.
+
+
+#include <iostream> 
+
+using namespace std; 
+
+int findMissing(int arr[], int N) 
+{ 
+	int left = 0, right = N - 1; 
+	while (left <= right) { 
+
+		int mid = (left + right) / 2; 
+
+		// If the middle element is not on
+		// the middle index, then the missing element is mid + 1.
+		if (arr[mid] != mid + 1 && arr[mid - 1] == mid) 
+		{
+            return mid + 1;          
+        }
+
+		// This case means that the missing element is
+		// not to the left. So search the right.	
+		if (arr[mid] == mid + 1) 
+			left = mid - 1; 
+
+		// This case means that the missing element is not
+		// to the right. So search the left.
+		else
+			left = mid + 1; 
+	} 
+
+	// Will reach here if no missing element found.  
+	return -1; 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int arr[] = {1,2,3,4,6,7,8}; 
+	int size = sizeof(arr)/sizeof(arr[0]); 
+	cout << "The missing element is: " << findMissing(arr, size); 
+	return 0; 
+} 
